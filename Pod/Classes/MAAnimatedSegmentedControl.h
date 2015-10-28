3cde1f968c9b1f3a548cdbbe531c93a94704f1d0
Aleksandar Kex Trpeski <atrpeski.kex@gmail.com>
2015-10-28 18:16:26 +0100

    Changed selectedIndex to be publicly assignable property.

--- Pod/Classes/MAAnimatedSegmentedControl.h
+++ Pod/Classes/MAAnimatedSegmentedControl.h
@@ -15,11 +15,11 @@ IB_DESIGNABLE
 @property (nonatomic, copy) UIFont *font;
 @property (nonatomic, copy) NSArray *items;
 
-@property (nonatomic, readonly) IBInspectable NSUInteger selectedIndex;
+@property (nonatomic, assign) IBInspectable NSUInteger selectedIndex;
 
 @property (nonatomic, copy) IBInspectable UIColor *selectedLabelColor;
 @property (nonatomic, copy) IBInspectable UIColor *unselectedLabelColor;
 @property (nonatomic, copy) IBInspectable UIColor *thumbColor;
-@property (nonatomic, copy) IBInspectable UIColor *borderColor;
 
+@property (nonatomic, copy) IBInspectable UIColor *borderColor;
 @end
